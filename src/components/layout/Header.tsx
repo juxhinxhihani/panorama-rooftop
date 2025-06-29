cfdfcbe9a978a7593e79840200cc88290f757d80
Juxhin Xhihani <juxhinxhihani@gmail.com>
2025-06-30 01:43:00 +0200

    Add menu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
 
   const navItems = [
     { href: '#about', label: 'About' },
-    { href: '#menu', label: 'Menu' },
+    { href: '/menu', label: 'Menu' },
     { href: '#contact', label: 'Contact' }
   ]
 
