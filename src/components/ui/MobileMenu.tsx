fd831604d03f9fef754a268952c3fbc7a2810811
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-30 01:53:30 +0200

    Make Navbar More Beautiful

--- src/components/ui/MobileMenu.tsx
+++ src/components/ui/MobileMenu.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { useEffect } from 'react'
 
 interface MobileMenuProps {
@@ -10,8 +9,6 @@ interface MobileMenuProps {
 }
 
 export default function MobileMenu({ isOpen, onClose, navItems }: MobileMenuProps) {
-  const pathname = usePathname()
-
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden'
