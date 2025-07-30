2595c88d1d9107bc1f9edc008c4313c0a1e6ba28
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-30 09:42:43 +0200

    Add Background Image to Hero Section

--- src/screens/HeroScreen.tsx
+++ src/screens/HeroScreen.tsx
@@ -9,10 +9,10 @@ export default function HeroScreen() {
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
       {/* Background Image */}
       <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-          style={{
-            backgroundImage: "url('/images/hero.jpg')"
-          }}
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: "url('/images/hero.jpg')"
+        }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-40" />
       </div>
