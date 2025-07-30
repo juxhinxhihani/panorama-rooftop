c57926c6739dc349672860885f85b06497993b9b
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-30 09:41:58 +0200

    Fix Hero Arrow Down Position

--- src/screens/HeroScreen.tsx
+++ src/screens/HeroScreen.tsx
@@ -47,7 +47,7 @@ export default function HeroScreen() {
         </div>
 
         {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce z-20">
           <svg className="w-6 h-6 text-amber-200/80 drop-shadow-md" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
           </svg>
