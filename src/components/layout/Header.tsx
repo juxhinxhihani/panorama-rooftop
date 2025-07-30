b15f0ee9a96632c1335519f6f1e370f63bf7d587
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-30 09:28:13 +0200

    Set Title Margin Left to -ml-3

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
                 isScrolled || pathname !== '/' ? 'text-gray-900' : 'text-white'
               } ${
                 isScrolled || pathname !== '/' ? 'block' : 'hidden md:block'
-              }`}>
+              } -ml-3`}>
                 Panorama Rooftop
               </span>
             </Link>
