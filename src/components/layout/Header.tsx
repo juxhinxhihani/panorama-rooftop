1e78119f91bfc4b9b359299af6878e2509deaf9a
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-30 02:12:57 +0200

    Make Icon and Title Closer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -35,6 +35,7 @@ export default function Header() {
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link href="/" className="flex items-center space-x-2 group">
+            <Link href="/" className="flex items-center space-x-1 group">
               <div className="relative w-16 h-16 transform group-hover:scale-110 transition-all duration-300 overflow-hidden rounded-full">
                 <img
                   src="/images/image copy copy.png"
