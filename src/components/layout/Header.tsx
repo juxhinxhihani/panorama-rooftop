63dc24a9fdccd01edd33eea07183c1ffa16deb92
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-30 09:26:02 +0200

    Make Header Title Text Even Smaller

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
               } ${
                 isScrolled || pathname !== '/' ? 'block' : 'hidden md:block'
               }`}>
-                Panorama Rooftop
+               <span className="text-sm">Panorama Rooftop</span>
               </span>
             </Link>
 
